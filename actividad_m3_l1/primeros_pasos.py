17e3359bae675839e1c32153fc3665bde035f5dd
Sebastián Torres <44385105+sebatorreso@users.noreply.github.com>
2026-03-04 20:19:41 +0000

    Se resulve el Ejercicio 1 del Módulo Fundamentos de Python

---
+++ actividad_m3_l1/primeros_pasos.py
@@ -0,0 +1,26 @@
+#-----------------------------------------------
+# ACTIVIDAD N°1 - FUNDAMENTOS DE LENGUAJE PYTHON
+#-----------------------------------------------
+# Importando librería math()
+import math
+
+# 1. Imprimir un mensaje inicial
+print('¡Hola! Este es mi primer programa en Python.')
+
+# 2. Declara variables simples y muestra su contenido con print()
+nombre = 'Sebastián'
+edad = 35
+ciudad = 'Concepción'
+print('Hola me llamo ',nombre, 'tengo ',edad, 'y soy de ',ciudad)
+
+# 3. Realizar operaciones básicas
+# Suma dos números y muestra el resultado.
+n1 = 5 # Se declara la variable n1 con valor 5
+n2 = 7 # Se declara la variable n2 con valor 7
+print('La suma de las variables n1 y n2 es:', n1+n2) # Se imprime resulado por pantalla
+
+# 4. Explorar un módulo integrado
+# Importar el módulo math y utiliza la función sqrt()
+numero = 25 # Se declara la variable 'numero' con valor 25
+raiz_numero = math.sqrt(numero) # Se calcula la razi cuadrada de 'numero' y se asigna resultado a variable 'raiz_numero'
+print('La raiz cuadrada de',numero,'es',raiz_numero) # Se imprime el resultado por pantalla
